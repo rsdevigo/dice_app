d07a0624800ef3621a1d5cf3e2c77432a1007d5e
Rodrigo Devigo <rodrigo.devigo@ifms.edu.br>
2018-09-14 10:06:44 -0400

    Criando propriedade para o dado

--- src/components/dice.js
+++ src/components/dice.js
@@ -8,17 +8,34 @@ export default class Dice extends React.Component {
     this.state = {number: 1};
   }
   render() {
+    let dado;
+    if (this.state.number == 1) {
+      dado = <Image source={require('./numero1.png')} style={styles.dice} />
+    } else if (this.state.number == 2) {
+      dado = <Image source={require('./numero2.png')} style={styles.dice} />
+    } else if (this.state.number == 3) {
+      dado = <Image source={require('./numero3.png')} style={styles.dice} />
+    } else if (this.state.number == 4) {
+      dado = <Image source={require('./numero4.png')} style={styles.dice} />
+    } else if (this.state.number == 5) {
+      dado = <Image source={require('./numero5.png')} style={styles.dice} />
+    } else if (this.state.number == 6) {
+      dado = <Image source={require('./numero6.png')} style={styles.dice} />
+    }
 
     return (
       <View style={styles.body}>
-        <Text>Aqui estamos mais um dia</Text>
+        {dado}
+        <Button title="" onPress={this._rollDice.bind(this)} />
       </View>
     );
 
   }
 
   _rollDice () {
-
+    let sorteio = Math.floor(Math.random()*6)+1;
+    console.log(sorteio);
+    this.setState({number: sorteio});
   }
 }
 
